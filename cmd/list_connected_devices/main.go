e98a0ce73ad42e4d7565cc1164ab6db4caa9119c
Tobias <tobias@olsson.be>
2021-12-27 19:45:18 +0100

    Trying to get vid & pid in log

--- cmd/list_connected_devices/main.go
+++ cmd/list_connected_devices/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"gopkg.in/yaml.v3"
 	"hotplug-proxmox-devices/lib"
 	"log"
 	"os"
 )
+
+type ConnectedDeviceWithVidPid struct {
+	// e.g. auto_0
+	ConnectedName string `yaml:"connectedName"`
+	QomTreePath   string `yaml:"qomTreePath"`
+	OtherPath     string `yaml:"otherPath"`
+	BusAndPort    string `yaml:"busAndPort"`
+	VidPid        string `yaml:"vidPid"`
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -21,6 +31,33 @@ func main() {
 		Monitor: tmp,
 		VmId:    vmId,
 	}
+	scannedDevices := lib.ScanUsbDevices()
 	connectedDevices := lib.ListConnectedDevices(connection)
-	yaml.NewEncoder(log.Writer()).Encode(connectedDevices)
+	devices := make([]ConnectedDeviceWithVidPid, len(connectedDevices))
+	for idx, device := range devices {
+		var vidPid string
+		for _, scannedDevice := range scannedDevices {
+			busAndPort := fmt.Sprintf("%s-%s", scannedDevice.BusId, scannedDevice.PortPath)
+			if busAndPort == device.BusAndPort {
+				vidPid = device.VidPid
+				break
+			} else {
+				log.Printf("%s vs %s", busAndPort, device.BusAndPort)
+			}
+		}
+
+		devices[idx] = ConnectedDeviceWithVidPid{
+			ConnectedName: connectedDevices[idx].ConnectedName,
+			QomTreePath:   connectedDevices[idx].ConnectedName,
+			OtherPath:     connectedDevices[idx].ConnectedName,
+			BusAndPort:    connectedDevices[idx].ConnectedName,
+			VidPid:        vidPid,
+		}
+	}
+
+	err = yaml.NewEncoder(log.Writer()).Encode(connectedDevices)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
 }
